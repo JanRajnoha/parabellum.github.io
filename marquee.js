e2084d1f459c5cbcdfb0bffa72dcf333ff78a212
Jan Rajnoha <janrajnoha@outlook.com>
2020-09-06 14:55:52 +0200

    update

--- marquee.js
+++ marquee.js
@@ -1,11 +1,99 @@
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.marquee-content");
-
+/*const defaultElementsCount = NumberOfDefaultElements();*/
 root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+root.style.setProperty("--marquee-width", marqueeContent.offsetWidth);
 
-for(let i=0; i<marqueeElementsDisplayed; i++) {
+
+for(let i=0; i<marqueeElementsDisplayed; i++) 
+{
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
 
-//CallLozadObserver();
+
+
+
+
+
+
+
+
+
+
+/*
+var elementsWidth = SummaryElementsWidth(numberOfElements);
+var screenWidth = window.innerWidth;
+
+DuplicateElements(elementsWidth, screenWidth, numberOfElements)
+ExtendDisplayedElements(screenWidth)
+
+function NumberOfDefaultElements()
+{
+    return document.getElementsByClassName("marquee-item");
+}
+
+function DuplicateElements(elementsWidth, screenWidth, numberOfElements)
+{
+    var numberOfRepeats = NumberOfRepeatsNeededForDuplicating(elementsWidth, screenWidth);
+
+    for (let i = 1; i <= numberOfRepeats; i++)
+    {
+        for(let i=0; i < numberOfElements; i++) {
+          marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+        }  
+    }
+}
+
+function NumberOfRepeatsNeededForDuplicating(elementsWidth, screenWidth)
+{
+    var numberOfRepeats = 0;
+    var allElementsWidth = elementsWidth
+
+    while (allElementsWidth < screenWidth)
+    {
+        numberOfRepeats++;
+        allElementsWidth += elementsWidth;
+    }
+
+    return numberOfRepeats;
+}
+
+function SummaryElementsWidth(numberOfElements) 
+{
+    var summaryWidth = 0;
+
+    for (let i = 0; i < numberOfElements.length; i++) 
+    {
+        summaryWidth += numberOfElements[i].offsetWidth;
+    }
+
+    return summaryWidth;
+}
+
+function ExtendDisplayedElements(screenWidth)
+{
+    var showedElementsWidth = GetShowedElementsWidth(screenWidth);
+}
+
+function GetShowedElementsWidth(screenWidth)
+{
+    var allElements = NumberOfDefaultElements();
+    var showedElementsWidth = 0;
+    var index = 0;
+
+    while (showedElementsWidth + allElements[index].offsetWidth < screenWidth)
+    {
+        let element = allElements[index];
+        showedElementsWidth = element.offsetWidth;
+        index++;
+    }
+
+    if (showedElementsWidth >= screenWidth)
+    {
+        index--;
+    }
+
+    return showedElementsWidth
+}
+*/
