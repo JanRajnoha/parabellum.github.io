92c811007b74c45e221ee67d1b43acc3c290f0ac
Jan Rajnoha <janrajnoha@outlook.com>
2020-09-06 17:04:12 +0200

    update js

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 WebFont.load({ google: { families: ["Roboto Condensed:regular,700"] } });
 
 window.onload = (event) => {
-    //const observer = window.lozad(); // lazy loads elements with default selector as '.lozad'
-    //observer.observe();
+    ComputeMarqueeWidth();
 };
-
-function CallLozadObserver()
-{
-    observer.observe();
-}
--- marquee.js
+++ marquee.js
@@ -2,7 +2,6 @@ const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.marquee-content");
 root.style.setProperty("--marquee-elements", marqueeContent.children.length);
-root.style.setProperty("--marquee-width", GetWidthOfDefaultItems().toString().concat("px"));
 
 let index = 0;
 for(let i=0; i<marqueeElementsDisplayed; i++) 
@@ -27,6 +26,11 @@ function GetWidthOfDefaultItems()
     }
 
     return defaultElementsWidth;
+}
+
+function ComputeMarqueeWidth()
+{
+    root.style.setProperty("--marquee-width", GetWidthOfDefaultItems().toString().concat("px"));
 }
 
 
