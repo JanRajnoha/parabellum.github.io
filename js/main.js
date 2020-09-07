01b6ffe9b09531ab98544ffb6c517692a8e4fb5e
Jan Rajnoha <janrajnoha@outlook.com>
2020-09-07 21:17:59 +0200

    update js

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ WebFont.load({ google: { families: ["Roboto Condensed:regular,700"] } });
 window.onload = (event) => {
 /*    ComputeMarqueeWidth();
     ReRunMarquee();*/
-};
 
-$(function() {
-    $('.demo').imageslider({
-    slideItems: '.my-slider-item',
-    slideContainer: '.my-slider-list',
-    slideDistance: 5,
-    slideDuratin: 800,
-    resizable: true,
-    pause: true
+    $(function() {
+        $('.de').imageslider({
+        slideItems: '.my-slider-item',
+        slideContainer: '.my-slider-list',
+        slideDistance: 5,
+        slideDuratin: 800,
+        resizable: true,
+        pause: true
+        });
     });
-    });
+};
