b8aa39a7745a22522517cb1aac3675f1423a126e
Jan Rajnoha <janrajnoha@outlook.com>
2020-09-07 22:09:08 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 WebFont.load({ google: { families: ["Roboto Condensed:regular,700"] } });
 
 window.onload = (event) => {
+    RunMarquee();
 };
+
+function RunMarquee()
+{
+    /*$(function() {
+        $('.de').imageslider({
+            slideItems: '.my-slider-item',
+            slideContainer: '.marquee-content',
+            slideDistance: 5,
+            slideDuratin: 800,
+            resizable: true,
+            pause: true
+        });
+    });*/
+}
